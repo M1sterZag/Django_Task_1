abc3b003717132ad4361176822d25f822d564ee3
Maxim Zaglubotsky <91866807+M1sterZag@users.noreply.github.com>
2024-09-11 17:46:57 +0000

    add, edit tasks funcs done

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,8 @@ app_name = 'tasks'
 urlpatterns = [
     path("", views.index, name="index"),
     path("tasks/", views.tasks, name="tasks"),
-    path("tasks/<int:task_id>/", views.task, name="task")
+    path("tasks/<int:task_id>/", views.task, name="task"),
+    path("new_task/", views.new_task, name="new_task"),
+    path('edit_task/<int:task_id>/', views.edit_task, name='edit_task'),
+    # path('delete_task/<int:task_id>/', views.delete_task, name='delete_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
@@ -19,3 +20,40 @@ def task(request, task_id):
     #     raise Http404
     context = {'task': task}
     return render(request, 'tasks/task.html', context)
+
+
+def new_task(request):
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = TaskForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:tasks')
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'tasks/new_task.html', context)
+
+
+def edit_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method != 'POST':
+        # Исходный запрос; форма заполняется данными текущей записи.
+        form = TaskForm(instance=task)
+    else:
+        # Отправка данных POST; обработать данные.
+        form = TaskForm(instance=task, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:task', task_id=task.id)
+    context = {'task': task, 'form': form}
+    return render(request, 'tasks/edit_task.html', context)
+
+
+# def delete_task(request, task_id):
+#     print(f"Attempting to delete task with id: {task_id}")
+#     task = get_object_or_404(Task, id=task_id)
+#     task.delete()
+#     return redirect("tasks:tasks")
