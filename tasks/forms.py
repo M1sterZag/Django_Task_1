6fcb73966502141718f80cc15cf786cdbda81e1e
Maxim Zaglubotsky <91866807+M1sterZag@users.noreply.github.com>
2024-10-21 11:20:10 +0000

    changed styles

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Task
 
-
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ("title", "text", "end_date", "priority")
+        fields = ['title', 'text', 'end_date', 'priority']
+
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
+        self.fields['text'].widget.attrs.update({'class': 'form-control'})
+        self.fields['end_date'].widget.attrs.update({'class': 'form-control'})
+        self.fields['priority'].widget.attrs.update({'class': 'form-control'})
