a8a4211e9f6e70cba34132b63acfd419d62e5ce3
Maxim Zaglubotsky <91866807+M1sterZag@users.noreply.github.com>
2024-12-17 20:35:21 +0000

    modified:   TodoList/settings.py

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-s0j%^ghl@wjof=m)j=2mri+34!i^)ag*31+blx5c2ipeombqq("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
